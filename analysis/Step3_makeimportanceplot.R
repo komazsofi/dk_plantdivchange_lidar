5eb4a6fc2f3434171b53d92effd68ed71bc96db4
komazsofi <komazsofi@gmail.com>
2022-08-24 14:48:44 +0200

    start config fea imp plot

--- analysis/Step3_makeimportanceplot.R
+++ analysis/Step3_makeimportanceplot.R
@@ -8,12 +8,12 @@ library(grid)
 
 # Import RF objects
 
-setwd("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/PlantDivChange_lidar/processing/")
+setwd("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/PlantDivChange_lidar/processing_082022/")
 
 ## Nature dry
 
-rf_natdry_2007=readRDS("rf_natdry_2007_cv100.rds")
-rf_natdry_2015=readRDS("rf_natdry_2015_cv100.rds")
+rf_natdry_2007=readRDS("rf_natdry_2007_rep10cv10_230822.rds")
+rf_natdry_2015=readRDS("rf_natdry_2015_rep10cv10_230822.rds")
 
 feaimp_natdry_df_2007=as.data.frame(rf_natdry_2007[["finalModel"]][["importance"]])
 feaimp_natdry_df_2007$metrics <- c("veg_vert_sd","het_prop_shrub","het_height_sd","het_veg_dens_sd","topo_slope","topo_solar_rad","topo_twi",
@@ -32,7 +32,7 @@ names(feaimp_natdry_df_2007)[1] <- "meanImp"
 feaimp_natdry_df_sort_2007 <- feaimp_natdry_df_2007[order(feaimp_natdry_df_2007$meanImp,decreasing = TRUE), ]
 feaimp_natdry_df_sort_n5_2007=head(feaimp_natdry_df_sort_2007,n=5)
 
-p1=ggplot(feaimp_natdry_df_2007, aes(x=metrics, y=meanImp, color=as.factor(FeaGroup))) + 
+p1=ggplot(feaimp_natdry_df_sort_2007, aes(x=metrics, y=meanImp, color=as.factor(FeaGroup))) + 
   geom_point(size=5,show.legend = FALSE) +
   geom_segment(aes(x=metrics,xend=metrics,y=0,yend=meanImp),size=3,show.legend = FALSE) +
   ylab("%IncMSE") +
@@ -96,4 +96,4 @@ fig_natdry=grid.arrange(
   widths = c(1,1)
 )
 
-ggsave("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/PlantDivChange_lidar/_forposter/fig_natdry.png",plot = fig_natdry,width = 32, height =15)
+ggsave("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/PlantDivChange_lidar/processing_082022/fig_natdry.png",plot = fig_natdry,width = 32, height =15)
